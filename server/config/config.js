067c9db96172aa720898651ac8c1108a41c7171b
GeorgeBeta <jorge@zaporta.com>
2018-11-24 20:59:05 +0100

    Punto 107 - Variables de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,14 @@
 // *************************************
 
 process.env.PORT = process.env.PORT || 3000;
+
+// *************************************
+//    Entorno y BBDD a mi manera
+// *************************************
+
+if (process.env.NODE_ENV === 'production') {
+    urlDB = process.env.MONGO_URI;
+} else {
+    urlDB = 'mongodb://localhost:27017/cafe';
+}
+process.env.URLDB = urlDB;
