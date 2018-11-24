a6262a34b1aa941d6d920dfcdb7b2b66f0b68174
GeorgeBeta <jorge@zaporta.com>
2018-11-24 21:05:47 +0100

    Punto 107 - Variables de entorno v2

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.listen(process.env.PORT, () => {
     console.log('Escuchando el puerto :', process.env.PORT);
 })
 
-mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
+mongoose.connect(process.env.URLDB, (err, res) => {
     if (err) throw err;
     console.log('Base de datos ONLINE');
 });
